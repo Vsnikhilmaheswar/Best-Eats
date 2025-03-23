0ccec73be27fb35b5eda1f41edc984f3e845853e
Vsnikhilmaheswar <nikhilmaheswar12@gmail.com>
2025-03-23 11:06:42 +0530

    Food section done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <NavBar/>
       <Hero/>
       <HeadlineCards/>
+      <Food/>
     </>
   )
 }
